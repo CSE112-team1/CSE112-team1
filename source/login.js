10c233c574bf9fcc6463e5209d43b776a6067453
RhythmGrind <Z001026Z@GMAIL.COM>
2024-05-13 03:30:12 -0700

    lint

--- source/login.js
+++ source/login.js
@@ -1,30 +1,30 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-analytics.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js';
+// import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-analytics.js';
+import { getAuth, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAg-V91iKbuab9_xlFqMLFVIDmLmJ_5WrE",
-  authDomain: "cse-112-tarot-card-reader.firebaseapp.com",
-  projectId: "cse-112-tarot-card-reader",
-  storageBucket: "cse-112-tarot-card-reader.appspot.com",
-  messagingSenderId: "610618527051",
-  appId: "1:610618527051:web:b803c1458dd133bc6c6cce",
-  measurementId: "G-J13L8M0WRX"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: 'cse-112-tarot-card-reader.firebaseapp.com',
+  projectId: 'cse-112-tarot-card-reader',
+  storageBucket: 'cse-112-tarot-card-reader.appspot.com',
+  messagingSenderId: '610618527051',
+  appId: '1:610618527051:web:b803c1458dd133bc6c6cce',
+  measurementId: 'G-J13L8M0WRX'
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 //sign-in button
 const login = document.getElementById('login-button');
-login.addEventListener("click", function(event){
-  event.preventDefault()
+login.addEventListener('click', function(event){
+  event.preventDefault();
 
   //inputs
   const email = document.getElementById('login').value;
@@ -34,14 +34,15 @@ login.addEventListener("click", function(event){
   signInWithEmailAndPassword(auth, email, password)
   .then((userCredential) => {
     // Signed in 
-    const user = userCredential.user;
-    alert("logged in successfully")
+    // const user = userCredential.user;
+    console.log('User created:', userCredential.user);
+    alert('logged in successfully');
     // ...
   })
   .catch((error) => {
-    const errorCode = error.code;
     const errorMessage = error.message;
-    alert(errorMessage)
+    alert(errorMessage);
+    console.log(error.code, error.message);
   });
 
-})
+});
--- source/signup.js
+++ source/signup.js
@@ -1,29 +1,29 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-analytics.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js';
+// import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-analytics.js';
+import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAg-V91iKbuab9_xlFqMLFVIDmLmJ_5WrE",
-  authDomain: "cse-112-tarot-card-reader.firebaseapp.com",
-  projectId: "cse-112-tarot-card-reader",
-  storageBucket: "cse-112-tarot-card-reader.appspot.com",
-  messagingSenderId: "610618527051",
-  appId: "1:610618527051:web:b803c1458dd133bc6c6cce",
-  measurementId: "G-J13L8M0WRX"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: 'cse-112-tarot-card-reader.firebaseapp.com',
+  projectId: 'cse-112-tarot-card-reader',
+  storageBucket: 'cse-112-tarot-card-reader.appspot.com',
+  messagingSenderId: '610618527051',
+  appId: '1:610618527051:web:b803c1458dd133bc6c6cce',
+  measurementId: 'G-J13L8M0WRX'
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 //register button
 const register = document.getElementById('signup-button');
-register.addEventListener("click", function (event) {
-  event.preventDefault()
+register.addEventListener('click', function (event) {
+  event.preventDefault();
 
   //inputs
   const email = document.getElementById('login').value;
@@ -33,14 +33,15 @@ register.addEventListener("click", function (event) {
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       // Signed up 
-      const user = userCredential.user;
-      alert("Creating Account...")
+      // const user = userCredential.user;
+      console.log('User created:', userCredential.user);
+      alert('Creating Account...');
       // ...
     })
     .catch((error) => {
-      const errorCode = error.code;
       const errorMessage = error.message;
-      alert(errorMessage)
+      alert(errorMessage);
+      console.log(error.code, error.message);
       // ..
     });
-})
+});
