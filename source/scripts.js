7c126f0248385acf62b77d6eb8ac29b3fa7f2f12
Christopher Hughes <clhughes@ucsd.edu>
2024-05-16 12:48:40 -0700

    Adds js and html for daily fortune error popup

--- source/scripts.js
+++ source/scripts.js
@@ -384,6 +384,14 @@ function displayHomeScreen() {
  * and show all elements which we do want displayed
  */
 function displayDailyFortuneScreen() {
+  const auth = getAuth();
+  if (!auth.currentUser) {
+    document.getElementById('login-popup').style.display = 'flex';  // Show the login popup if not logged in
+    return;  // Stop the function if not logged in
+  }
+
+  // If logged in, hide the popup if it was previously shown
+  document.getElementById('login-popup').style.display = 'none';
   // hide history section
   document.getElementById('history-section').hidden = true;
   // change card images back to their defaults
