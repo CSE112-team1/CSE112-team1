4c72a558de4406431b1fcdc2cad18bdf8ade9c90
RhythmGrind <Z001026Z@GMAIL.COM>
2024-05-20 22:06:10 -0700

    update_linter

--- source/__tests__/integration/login.test.js
+++ source/__tests__/integration/login.test.js
@@ -2,8 +2,8 @@ function mockGetAuth() {
   return 'mockAuth123'; 
 } 
 function mockSignInWithEmailAndPassword(auth,user,password) {
-  if (auth == "mockAuth123") { 
-    if (user == "mock@gmail.com" && password == "mocking123") { 
+  if (auth == 'mockAuth123') { 
+    if (user == 'mock@gmail.com' && password == 'mocking123') { 
       return new Promise((resolve) => {resolve('success');}); 
     }
     return new Promise((reject) => {reject('Invalid Creds!');}); 
@@ -19,8 +19,8 @@ describe('Login Scenario', () => {
     await page.goto('http://127.0.0.1:5500/source/login.html'); 
   }); 
   it('Incorrect Email', async () => { 
-    await page.evaluate( () => document.getElementById("login").value = "") 
-    await page.evaluate( () => document.getElementById("password").value = "") 
+    await page.evaluate( () => document.getElementById('login').value = ''); 
+    await page.evaluate( () => document.getElementById('password').value = ''); 
     await page.type('input#login', 'fail@gmail.com'); 
     var email = await page.$eval('input#login', input => input.value);
 
@@ -28,19 +28,19 @@ describe('Login Scenario', () => {
     var pass = await page.$eval('input#password', input => input.value);
 
     const auth = mockGetAuth(); 
-    var message = ""; 
+    var message = ''; 
     await mockSignInWithEmailAndPassword(auth, email, pass).then((result) => {
       // Signed in 
       message = result; 
     }).catch((error) => {
-      // wrong 
-      message = "failed"; 
+        console.error(error); 
+      message = 'failed'; 
     }); 
-    await expect(message).toBe("Invalid Creds!"); 
+    await expect(message).toBe('Invalid Creds!'); 
   }); 
   it('Incorrect Password', async () => { 
-    await page.evaluate( () => document.getElementById("login").value = "") 
-    await page.evaluate( () => document.getElementById("password").value = "") 
+    await page.evaluate( () => document.getElementById('login').value = ''); 
+    await page.evaluate( () => document.getElementById('password').value = ''); 
     await page.type('input#login', 'mock@gmail.com'); 
     var email = await page.$eval('input#login', input => input.value);
 
@@ -48,19 +48,19 @@ describe('Login Scenario', () => {
     var pass = await page.$eval('input#password', input => input.value);
 
     const auth = mockGetAuth(); 
-    var message = ""; 
+    var message = ''; 
     await mockSignInWithEmailAndPassword(auth, email, pass).then((result) => {
       // Signed in 
       message = result; 
     }).catch((error) => {
-      // wrong 
-      message = "failed"; 
+        console.error(error); 
+      message = 'failed'; 
     }); 
-    await expect(message).toBe("Invalid Creds!"); 
+    await expect(message).toBe('Invalid Creds!'); 
   }); 
   it('Correct values', async () => { 
-    await page.evaluate( () => document.getElementById("login").value = "") 
-    await page.evaluate( () => document.getElementById("password").value = "") 
+    await page.evaluate( () => document.getElementById('login').value = ''); 
+    await page.evaluate( () => document.getElementById('password').value = ''); 
     await page.type('input#login', 'mock@gmail.com'); 
     var email = await page.$eval('input#login', input => input.value);
 
@@ -68,14 +68,14 @@ describe('Login Scenario', () => {
     var pass = await page.$eval('input#password', input => input.value);
 
     const auth = mockGetAuth(); 
-    var message = ""; 
+    var message = ''; 
     await mockSignInWithEmailAndPassword(auth, email, pass).then((result) => {
       // Signed in 
       message = result; 
     }).catch((error) => {
-      // wrong 
-      message = "failed"; 
+        console.error(error);
+      message = 'failed'; 
     }); 
-    await expect(message).toBe("success"); 
+    await expect(message).toBe('success'); 
   }); 
 }); 
